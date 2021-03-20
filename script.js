c0963aab1fa0972f33027a7e86200486aafd430e
PabloESaavedra <pablosaa92@gmail.com>
2021-03-20 19:09:49 -0300

    final commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Add selector
+
+document.querySelectorAll(".question").forEach((question) => question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")){
+        question.parentNode.classList.toggle("active")
+    }
+    else{
+        document.querySelectorAll(".question").forEach(question => question.parentNode.classList.remove("active"));
+        question.parentNode.classList.add("active")
+    }
+
+}))
+
+/*
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
+
+    }))
+}) */
